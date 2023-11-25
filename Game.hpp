e5c0ede78f350278cccaa4f63780f19d0d140d0a
Sajal Fatima <122104042+savvy-tech-bySF@users.noreply.github.com>
2023-11-25 10:59:42 +0500

    Game screen updated

--- Game.hpp
+++ Game.hpp
@@ -1,13 +1,40 @@
 #pragma once
 #include "Player.hpp" 
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
 
 class Game {
+    //Screen dimension constants
+    const int SCREEN_WIDTH = 1000;
+    const int SCREEN_HEIGHT = 700;
+
+    //The window we'll be rendering to
+    SDL_Window* gWindow = NULL;
+
+    //The window renderer
+    SDL_Renderer* gRenderer = NULL;
+
+    //Current displayed texture
+    SDL_Texture* gTexture = NULL;
+    //global reference to png image sheets
+    SDL_Texture* assets=NULL;
+
 public:
     Game(); 
     void startGame(); 
     void playTurn(); 
     bool isGameOver(); 
     void displayWinner(); 
+    bool init();
+    bool loadMedia();
+    void close();
+    SDL_Texture* loadTexture( std::string path );
+    
 
 private:
     Player players[4]; // Assuming all 4 players are playing
--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #pragma once
+#include "Game.hpp"
 
-
-int main()
+int main(int argc, char* argv[])
 {
+    Game game;
+    srand(time(NULL));
+    if( !game.init() ){
+		printf( "Failed to initialize!\n" );
+        return 0;
+	}
+		//Load media
+    if( !game.loadMedia() ){
+        printf( "Failed to load media!\n" );
+        return 0;
+    }
+
+    game.startGame();
+    game.close();
     return 0;
 }
